9f5f157d44f8d7b3fab69ed5a01c634824914a92
ViniTex231 <126170717+ViniTex231@users.noreply.github.com>
2023-05-18 14:35:02 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,42 +1,56 @@
 import time
+import inquirer
 from random import choice
 from random import randint
 from pokedex import Grass, Water, Fire
+from colors import cores, bg, fx
+limpar = '\33[m'
+
 
 # PLANTAS
 bulbasaur = Grass("Bulbasaur", 11, 45, 45, 40)
+ivysaur = Grass("Ivysaur", 8, 47, 48, 40)
 
 # AGUA
 squirtle = Water("Squirtle", 10, 44, 46, 30)
+wartortle = Water("Wartortle", 11, 42, 45, 30)
 
 # FOGO
 charmander = Fire("Charmander", 9, 39, 65, 40)
+charizard = Fire("Charizard", 11, 42, 65, 40)
 
 
 def choosepokemon():
     print("-=" * 30)
-    print("Welcome to Pokemon Battle ⚔️")
-    print("You have to choose a Pokemon and the computer will choose a other Pokemon")
-    print(" ")
-    print("[1] - Bulbasaur")
-    print("[2] - Squirtle")
-    print("[3] - Charmander")
+    print(f"    {cores['preto']}{bg['vermelho']}Welcome to Pokemon Battle ⚔️{limpar}")
+    questions = [
+        inquirer.List('size',
+                      message=f"{cores['branco']}{fx['sublinhado']}You have to choose a Pokemon and the computer will choose a other Pokemon{limpar}",
+                      choices=[bulbasaur.name, ivysaur.name, charmander.name, charizard.name, squirtle.name, wartortle.name],
+                      ),
+    ]
+    answers = inquirer.prompt(questions)
+    option = answers['size']
     while True:
-        option = input("Choose your Pokemon: ")
-
-        if option == "1":
+        if option == bulbasaur.name:
             return bulbasaur
-        elif option == "2":
+        elif option == ivysaur.name:
+            return ivysaur
+        elif option == squirtle.name:
             return squirtle
-        elif option == "3":
+        elif option == wartortle.name:
+            return wartortle
+        elif option == charmander.name:
             return charmander
-        else:
-            print("Incorrect option")
+        elif option == charizard.name:
+            return charizard
 
 
 def randompc():
-    print("You choosed your Pokemon, now I choose my player")
-    pokemons = [bulbasaur, squirtle, charmander]
+    print(f"{fx['sublinhado']}You choosed your Pokemon, now I choose my player...{limpar}")
+    time.sleep(1)
+    print(" ")
+    pokemons = [bulbasaur, squirtle, charmander, charizard, wartortle, ivysaur]
     x = choice(pokemons)
     return x
 
@@ -48,35 +62,28 @@ def battle():
     pc_attack = x.attack
     pc_hp = x.hp
     pc_speed = x.speed
-    if x.name == "Bulbasaur":
-        pc_special = x.specialwater
-    elif x.name == "Squirtle":
-        pc_special = x.specialfire
-    else:
-        pc_special = x.specialgrass
-    # print(x.name)
+
     time.sleep(1)
-    print(f"My pokemon is {x.name}")
+    print(f"{fx['sublinhado']}My pokemon is {bg['amarelo']}{cores['preto']}{x.name}{limpar}")
+    print(" ")
     time.sleep(1)
 
     pokemon_name = p.name
     pokemon_attack = p.attack
     pokemon_hp = p.hp
     pokemon_speed = p.speed
-    if p.name == "Bulbasaur":
-        pokemon_special = p.specialwater
-    elif p.name == "Squirtle":
-        pokemon_special = p.specialfire
-    else:
-        pokemon_special = p.specialgrass
 
     if pokemon_speed > pc_speed:
-        print("Your pokemon have a overspeed, so you will start")
+        print(f"{fx['sublinhado']}Your pokemon have a overspeed, so you will start{limpar}")
         time.sleep(1)
         print(" ")
-        print(f"Your life is {pokemon_hp}")
+        print("-=" * 30)
         print(" ")
-        print(f"My life is {pc_hp}")
+        print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
+        print(" ")
+        print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
+        print(" ")
+        print("-=" * 30)
         print(" ")
 
         while True:
@@ -86,20 +93,20 @@ def battle():
                 print(" ")
                 time.sleep(2)
                 pc_hp = pc_hp - pokemon_attack
-                print(f"My life is {pc_hp}")
+                print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
                 if pc_hp <= 0:
                     time.sleep(1)
-                    print(f"My life is {pc_hp}, you win!")
+                    print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}, {bg['verde']}{cores['preto']}you win!{limpar}")
                     break
                 print("I will attack you!!")
                 print("Attacking...")
                 print(" ")
                 time.sleep(2)
                 pokemon_hp = pokemon_hp - pc_attack
-                print(f"Your life is {pokemon_hp}")
+                print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
                 if pokemon_hp <= 0:
                     time.sleep(1)
-                    print(f"Your life is {pokemon_hp}, I win!")
+                    print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}, I win!{limpar}")
                     break
 
     elif pokemon_speed == pc_speed:
@@ -117,20 +124,20 @@ def battle():
                     print(" ")
                     time.sleep(1)
                     pc_hp = pc_hp - pokemon_attack
-                    print(f"My life is {pc_hp}")
+                    print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
                     if pc_hp <= 0:
                         time.sleep(1)
-                        print(f"My life is {pc_hp}, you win!")
+                        print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}, {bg['verde']}{cores['preto']}you win!{limpar}")
                         break
                     print("I will attack you!!")
                     print("Attacking...")
                     print(" ")
                     time.sleep(1)
                     pokemon_hp = pokemon_hp - pc_attack
-                    print(f"Your life is {pokemon_hp}")
+                    print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
                     if pokemon_hp <= 0:
                         time.sleep(1)
-                        print(f"Your life is {pokemon_hp}, I win!")
+                        print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}, I win!{limpar}")
                         break
                 else:
                     print(" ")
@@ -141,10 +148,10 @@ def battle():
                 print(" ")
                 time.sleep(1)
                 pokemon_hp = pokemon_hp - pc_attack
-                print(f"Your life is {pokemon_hp}")
+                print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
                 if pokemon_hp <= 0:
                     time.sleep(1)
-                    print(f"Your life is {pokemon_hp}, I win!")
+                    print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}, I win!{limpar}")
                     break
                 attack = input("Type 1 for attack: ")
                 if attack == "1":
@@ -152,10 +159,10 @@ def battle():
                     print(" ")
                     time.sleep(1)
                     pc_hp = pc_hp - pokemon_attack
-                    print(f"My life is {pc_hp}")
+                    print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
                     if pc_hp <= 0:
                         time.sleep(1)
-                        print(f"My life is {pc_hp}, you win!")
+                        print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}, {bg['verde']}{cores['preto']}you win!{limpar}")
                         break
                 else:
                     print(" ")
@@ -163,19 +170,19 @@ def battle():
         print("My pokemon have a overspeed, so I will start")
         print(" ")
         time.sleep(1)
-        print(f"Your life is {pokemon_hp}")
+        print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
         print(" ")
-        print(f"My life is {pc_hp}")
+        print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
         print(" ")
         while True:
             print("Attacking...")
             print(" ")
             time.sleep(1)
             pokemon_hp = pokemon_hp - pc_attack
-            print(f"Your life is {pokemon_hp}")
+            print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}{limpar}")
             if pokemon_hp <= 0:
                 time.sleep(1)
-                print(f"Your life is {pokemon_hp}, I win!")
+                print(f"Your life is {bg['vermelho']}{cores['preto']}{pokemon_hp}, I win!{limpar}")
                 break
             attack = input("Type 1 for attack: ")
             if attack == "1":
@@ -183,10 +190,10 @@ def battle():
                 print(" ")
                 time.sleep(1)
                 pc_hp = pc_hp - pokemon_attack
-                print(f"My life is {pc_hp}")
+                print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}")
                 if pc_hp <= 0:
                     time.sleep(1)
-                    print(f"My life is {pc_hp}, you win!")
+                    print(f"My life is {bg['vermelho']}{cores['preto']}{pc_hp}{limpar}, {bg['verde']}{cores['preto']}you win!{limpar}")
                     break
             else:
                 print(" ")
